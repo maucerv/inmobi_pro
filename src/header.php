bd445bc5ab55eb57e9eb8624f51360f5616d7131
maucerv <mauricio.cervantes@sostic.com.mx>
2025-12-22 13:28:37 -0600

    Add files via upload

---
+++ src/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inmobiliaria Prestige</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" />
+    <link rel="stylesheet" href="css/estilos.css">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg sticky-top">
+        <div class="container">
+            <a class="navbar-brand" href="index.php">🏛 PRESTIGE REAL ESTATE</a>
+            <div class="collapse navbar-collapse justify-content-end">
+                <ul class="navbar-nav">
+                    <li class="nav-item"><a class="nav-link" href="index.php">Inicio</a></li>
+                    <li class="nav-item"><a class="nav-link" href="propiedades.php">Catálogo</a></li>
+                    <li class="nav-item"><a class="nav-link" href="mapa.php">Mapa Global</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
