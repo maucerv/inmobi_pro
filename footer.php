b484db5c5409ff6bf5020395ab607415cf3b9614
maucerv <mauricio.cervantes@sostic.com.mx>
2025-12-22 13:06:55 -0600

    Update footer.php

---
+++ footer.php
@@ -0,0 +1,9 @@
+<footer>
+        <div class="container">
+            <p>&copy; 2025 Prestige Real Estate. Desarrollado por Mauricio.</p>
+        </div>
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"></script>
+</body>
+</html>
