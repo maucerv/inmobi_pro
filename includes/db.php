ce516b52f9304cad73bff803f9cbf6e8b625fcc1
maucerv <mauricio.cervantes@sostic.com.mx>
2026-01-19 09:43:00 -0600

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -1,13 +1,17 @@
 <?php
-$host = 'localhost';
-$db = 'inmobiliaria_db';
-$user = 'root';
-$pass = ''; // En XAMPP por defecto la contraseña está vacía.
+// includes/db.php
+// Definimos que la base de datos es un archivo local
+$db_file = __DIR__ . '/../inmobiliaria.sqlite';
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
+    // Usamos el driver de SQLite en lugar de MySQL
+    $pdo = new PDO("sqlite:" . $db_file);
+    
+    // Configuración de errores y claves foráneas
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->exec("PRAGMA foreign_keys = ON;");
+
 } catch (PDOException $e) {
-    die("<h3>Error de conexión:</h3> " . $e->getMessage());
+    die("<h3>Error de sistema:</h3> " . $e->getMessage());
 }
 ?>
